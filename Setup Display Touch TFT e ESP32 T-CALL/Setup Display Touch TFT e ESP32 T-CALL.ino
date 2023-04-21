b190b74786fcdaef59906fcac4e764eb08929566
WDRETZER <walter_retzer@yahoo.com.br>
2023-04-21 13:48:45 -0300

    Inclusão do Setup Touch Display TFT e ESP32 T-CALL

--- Setup Display Touch TFT e ESP32 T-CALL/Setup Display Touch TFT e ESP32 T-CALL.ino
+++ Setup Display Touch TFT e ESP32 T-CALL/Setup Display Touch TFT e ESP32 T-CALL.ino
@@ -1,7 +1,7 @@
 /********************************************************************
   AUTOR: WALTER DAWID RETZER
   
-  PROJETO: SETUP DISPLAY TFT E ESP32 T-CALL
+  PROJETO: SETUP DISPLAY TOUCH TFT E ESP32 T-CALL
   
   VERSION: 1.0.0
   DATA: 21/04/2023
@@ -26,6 +26,11 @@
   PIN SCK           PIN 14
   PIN BL            PIN 22 OU 3V3
   PIN SDO (MISO)    NOT CONNECTED
+  PIN TCK           PIN 14
+  PIN TCS           PIN 19
+  PIN TDI (MOSI)    PIN 13 (MOSI/SDA/SDI)
+  PIN TDO (MISO)    PIN 12 (MISO/SDO)
+  PIN PEN           NOT CONNECTED
 
 ***********************************************************************/
 // Código:
@@ -36,7 +41,7 @@
 TFT_eSPI tft = TFT_eSPI();
 
 void setup(void) {
-  
+
   tft.init();
 
   tft.fillScreen(TFT_BLACK);
@@ -46,9 +51,6 @@ void setup(void) {
   tft.println("Hello World!!");
 
   delay(5000);
-}
-
-void loop() {
 
   tft.fillScreen(TFT_BLACK);
   delay(5000);
@@ -64,4 +66,19 @@ void loop() {
 
   tft.fillScreen(TFT_BLUE);
   delay(5000);
+}
+
+void loop() {
+
+  uint16_t x, y;
+
+  tft.getTouchRaw(&x, &y);
+
+  Serial.printf("x: %i     ", x);
+
+  Serial.printf("y: %i     ", y);
+
+  Serial.printf("z: %i \n", tft.getTouchRawZ());
+
+  delay(250);
 }
